5d0c4cac856147dd2c6f41910fbec6dcec37a889
Marco-Petronella <marco.petronella85@gmail.com>
2026-02-28 09:37:26 +0100

    track bonus and age inputs

---
+++ script2.js
@@ -0,0 +1,11 @@
+// età: chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+// pari o dispari: chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 9. Generare un numero casuale 
+// compreso tra 1 e 9, che sarà la giocata del computer. Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
+
+// quanto manca (OPZIONALE): utilizzando le funzioni dell'oggetto Date, stabilire l'ora attuale e attraverso una operazione matematica 
+// stabilire i minuti mancanti allo scoccare della prossima ora.
+
+const age1 = Number(prompt("inserisci l'età del primo viaggiatore"));
+const age2 = Number(prompt("inserisci l'età del secondo viaggiatore"));
+
