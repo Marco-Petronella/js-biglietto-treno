84fe439ba711b931973f01dded3a95d97b13c08c
Marco-Petronella <marco.petronella85@gmail.com>
2026-02-28 10:36:02 +0100

    fix variable name on script2

--- script2.js
+++ script2.js
@@ -8,59 +8,59 @@
 
 const age1 = Number(prompt("inserisci l'età del primo viaggiatore"));
 const age2 = Number(prompt("inserisci l'età del secondo viaggiatore"));
-let message;
+let messageExercise2;
 if (age1 < 0 || age2 < 0) {
-  message = "sono state inserite età non valide. " + age1 + " " + age2;
+  messageExercise2 = "sono state inserite età non valide. " + age1 + " " + age2;
 } else if (age1 > age2) {
-  message =
+  messageExercise2 =
     "il primo viaggiatore con " +
     age1 +
     "anni è il più vecchio, il secondo viaggiore ha " +
     age2 +
     "anni";
 } else if (age2 > age1) {
-  message =
+  messageExercise2 =
     "il secondo viaggiatore con " +
     age2 +
     "anni è il più vecchio, il primo viaggiore ha " +
     age1 +
     "anni";
 } else {
-  message =
+  messageExercise2 =
     "i due viaggiatori hanno entrambi la stessa età con " + age1 + "anni";
 }
-console.log(message);
+console.log(messageExercise2);
 
 const yourChoice = prompt("si vuole un numero pari o dispari?", "pari");
 const yourNumber = Number(prompt("scegliere un numero tra 1 e 9"));
 myNumber = parseInt(Math.random() * 9);
 
 if (yourChoice != "pari" && yourChoice != "dispari") {
-  message = "non è stato inserito un valore valido tra pari o dispari";
+  messageExercise2 = "non è stato inserito un valore valido tra pari o dispari";
 } else if (yourNumber < 1 || yourNumber > 9) {
-  message = "non è stato inserito un numero valido per il pari o dispari";
+  messageExercise2 = "non è stato inserito un numero valido per il pari o dispari";
 } else {
   oddOrEven = (yourNumber + myNumber) % 2;
   if (yourChoice == "pari")
     if (oddOrEven == 0) {
-      message = "hai scelto pari ed hai vinto";
+      messageExercise2 = "hai scelto pari ed hai vinto";
     } else {
-      message = "hai scelto pari ed hai perso";
+      messageExercise2 = "hai scelto pari ed hai perso";
     }
   else {
     if (oddOrEven == 0) {
-      message = "hai scelto dispari ed hai perso";
+      messageExercise2 = "hai scelto dispari ed hai perso";
     } else {
-      message = "hai scelto dispari ed hai vinto";
+      messageExercise2 = "hai scelto dispari ed hai vinto";
     }
   }
 }
-console.log(message);
+console.log(messageExercise2);
 console.log("mio numero=" + myNumber);
 console.log("tuo numero=" + yourNumber);
 console.log("tua scelta=" + yourChoice);
 
 const currentData = new Date();
 const toNextHour = 60 - (currentData.getMinutes() % 60);
-message = "alla prossima ora mancano " + toNextHour + " minuti";
-console.log(message);
+messageExercise2 = "alla prossima ora mancano " + toNextHour + " minuti";
+console.log(messageExercise2);
